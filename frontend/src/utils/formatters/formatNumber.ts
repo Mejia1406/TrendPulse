f99f828791b0274f82b43b90f7b6b584e0e20b0b
Sara Hurtado <shurtadom3@eafit.edu.co>
2026-08-30 21:37:41 -0500

    chore: add comments(names)

--- frontend/src/utils/formatters/formatNumber.ts
+++ frontend/src/utils/formatters/formatNumber.ts
@@ -1,16 +1,13 @@
+//Sara Hurtado y Samuel Moncada
 const numberWithSeparatorsFormatter = new Intl.NumberFormat('es-CO');
 
 export function formatNumber(value: number): string {
   if (value >= 1_000_000) {
-
     return `${(value / 1_000_000).toFixed(1)}M`;
   }
-
   if (value >= 1_000) {
-
     return `${Math.round(value / 1_000)}K`;
   }
-
   return value.toString();
 }
 
